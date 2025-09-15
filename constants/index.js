0efdcb06a0b62aa8133cc9f40cea747ba76c6350
AndreNot3000 <150428691+AndreNot3000@users.noreply.github.com>
2025-09-15 14:59:07 +0100

    first commit

---
+++ constants/index.js
@@ -0,0 +1,87 @@
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: '/bit1.jpg',
+    title: 'Bank-Grade Security',
+  },
+  {
+    id: 'world-2',
+    imgUrl: '/bit2.jpg',
+    title: 'Instant Settlements',
+  },
+  {
+    id: 'world-3',
+    imgUrl: '/planet-03.png',
+    title: 'Instant Settlements',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/planet-04.png',
+    title: 'All-in-One Platform',
+  },
+  {
+    id: 'world-5',
+    imgUrl: '/planet-05.png',
+    title: 'Advanced Trading Tools',
+  },
+];
+
+export const startingFeatures = [
+  'Buy and sell coins instantly, or trade securely via P2P',
+  'Trade anywhere, anytime — with support for multiple fiat currencies',
+  'Advanced charting, analytics, and APIs for professional traders',
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: 'Simplicity',
+    subtitle:
+        'we believe crypto trading should be simple, secure, and accessible to all',
+  },
+  {
+    imgUrl: '/headset.svg',
+    title: 'Diversified',
+    subtitle:
+        'Whether you’re buying your first Bitcoin or managing a diversified portfolio, GemX is built to support you',
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/planet-06.png',
+    title: 'Lorem ipsum dolor sit amet consectetur adipiscing elit :)',
+    subtitle:
+        'Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Diam maecenas sed enim ut sem viverra alique.',
+  },
+  {
+    imgUrl: '/planet-07.png',
+    title: 'Lorem ipsum dolor sit amet consectetur adipiscing elit :)',
+    subtitle:
+        'Vitae congue eu consequat ac felis donec. Et magnis dis parturient montes nascetur ridiculus mus. Convallis tellus id interdum',
+  },
+  {
+    imgUrl: '/planet-08.png',
+    title: 'Lorem ipsum dolor sit amet consectetur adipiscing elit :)',
+    subtitle:
+        'Quam quisque id diam vel quam elementum. Viverra nam libero justo laoreet sit amet cursus sit. Mauris in aliquam sem',
+  },
+];
+
+export const socials = [
+  {
+    name: 'twitter',
+    url: '/twitter.svg',
+    link: 'https://twitter.com/VigneshGupta9',
+  },
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+    link: 'https://www.linkedin.com/in/olumideandre/',
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+    link: 'https://www.instagram.com/AsapDre/',
+  },
+];
---
+++ sections/About.jsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { motion } from 'framer-motion';
+import { TypingText } from '../components';
+import styles from '../styles';
+import { fadeIn, staggerContainer } from '../utils/motion';
+
+const About = () => (
+  <section className={`${styles.paddings} relative z-10`}>
+    <div className="gradient-02 z-0" />
+
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
+    >
+      <TypingText
+        title="| About GemX"
+        textStyles="text-center"
+      />
+
+      <motion.p
+        variants={fadeIn('up', 'tween', 0.2, 1)}
+        className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
+      >
+        <span className="font-extrabold"> GemX </span> is a next-generation crypto exchange built for everyone — from beginners to pro traders., Our platform makes it simple to <span className="font-extrabold">buy and sell coins instantly</span> or trade securely through <span className="font-extrabold">peer-to-peer (P2P) transactions. </span> With advanced security, fast settlements, and 24/7 access, we’re redefining how you trade crypto <br /> <span className="font-extrabold">explore</span> our services by scrolling down
+      </motion.p>
+
+      <motion.img
+        variants={fadeIn('up', 'tween', 0.3, 1)}
+        src="/arrow-down.svg"
+        alt="arrow-down"
+        className="w-[18px] h-[28px] object-contain mt-[28px]"
+      />
+    </motion.div>
+  </section>
+);
+
+export default About;
